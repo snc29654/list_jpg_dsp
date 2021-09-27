71f860fc2102b771cf454fbe54a5e381243aa2b2
snc29654 <snc29654@nifty.com>
2021-09-28 08:18:51 +0900

    combox追加

--- file_or_folder_select.py
+++ file_or_folder_select.py
@@ -40,25 +40,37 @@ class image_gui():
         button3.grid(row=0, column=1)  
         button3.place(x=50, y=42) 
 
+        button4= Button(root_main, text=u'クリア', command=self.button4_clicked)  
+        button4.grid(row=0, column=1)  
+        button4.place(x=150, y=12) 
 
     def button1_clicked(self):  
+        global combovalue
 
         global filenames
         ini_dir = 'C:'
         ret = tkinter.filedialog.askdirectory(initialdir=ini_dir, title='file dialog test', mustexist = True)
         print(str(ret))
         os.chdir(str(ret))
-        filenames = glob.glob('*.jpg')
-        filenames.extend( glob.glob('*.txt'))
+        if(combovalue=="jpg"):
+            filenames = glob.glob('*.jpg')
+        if(combovalue=="txt"):
+            filenames = glob.glob('*.txt')
         self.textout()     
 
     def button3_clicked(self):  
         global filenames
-        
+
         fTyp = [('', '*')] 
         iDir = os.path.abspath(os.path.dirname(__file__)) 
-        filenames = tkFileDialog.askopenfilenames(filetypes= [("JPEG", ".jpg"),("TEXT", ".txt") ], initialdir=iDir)
+        if(combovalue=="jpg"):
+            filenames = tkFileDialog.askopenfilenames(filetypes= [("JPEG", ".jpg")], initialdir=iDir)
+        if(combovalue=="txt"):
+            filenames = tkFileDialog.askopenfilenames(filetypes= [("TEXT", ".txt") ], initialdir=iDir)
         self.textout()     
+
+    def button4_clicked(self):  
+        textExample.delete("1.0",tkinter.END)
 
     def textout(self):  
         global filenames
@@ -68,6 +80,16 @@ class image_gui():
 
     def quit(self):
         root_main.destroy()
+
+
+combovalue = "jpg"
+
+def show_selected(event):       #eventを引数に
+    global combovalue
+    combovalue=test_combobox.get()
+    print(combovalue)  #選択した値を表示
+
+
 
 root_main= tkinter.Tk()  
 image_gui(root_main)  
@@ -79,6 +101,24 @@ textExample.pack()
 textExample.place(x=90, y=70)
 
 textExample.delete("1.0",tkinter.END)
+
+
+item_list = ['jpg', 'txt']
+test_combobox = ttk.Combobox(
+    master=root_main,
+    values=item_list,
+    )
+
+#値選択時に発生するイベントと関数を紐づけ
+test_combobox.bind(
+    '<<ComboboxSelected>>',     #選択時に発生するイベント
+    show_selected,              #呼び出す関数
+)
+
+test_combobox.current(0)
+test_combobox.pack()
+
+
 
 root_main.mainloop()
 
