632bda28c7eadafb10c8c212fd0f2c93aa1f8777
snc29654 <snc29654@nifty.com>
2021-09-27 17:32:18 +0900

    textboxへ結果を表示する

--- file_or_folder_select.py
+++ file_or_folder_select.py
@@ -22,6 +22,7 @@ import glob
 from tkinter import filedialog as tkFileDialog
 import tkinter as tk
 from tkinter import font
+from tkinter.scrolledtext import ScrolledText
 
 filenames =[]
 select_file=[]
@@ -33,11 +34,11 @@ class image_gui():
         
         button1 = Button(root_main, text=u'フォルダー選択', command=self.button1_clicked)  
         button1.grid(row=0, column=1)  
-        button1.place(x=100, y=12) 
+        button1.place(x=50, y=12) 
 
         button3= Button(root_main, text=u'ファイル   選択', command=self.button3_clicked)  
         button3.grid(row=0, column=1)  
-        button3.place(x=100, y=42) 
+        button3.place(x=50, y=42) 
 
 
     def button1_clicked(self):  
@@ -48,8 +49,7 @@ class image_gui():
         print(str(ret))
         os.chdir(str(ret))
         filenames = glob.glob('*.jpg')
-        print(filenames)
-        self.quit()
+        textExample.insert(tkinter.END,filenames)
 
     def button3_clicked(self):  
         global filenames
@@ -57,8 +57,7 @@ class image_gui():
         fTyp = [('', '*')] 
         iDir = os.path.abspath(os.path.dirname(__file__)) 
         filenames = tkFileDialog.askopenfilenames(filetypes= [("JPEG", ".jpg") ], initialdir=iDir)
-        print(filenames)
-        self.quit()
+        textExample.insert(tkinter.END,filenames)
 
     def quit(self):
         root_main.destroy()
@@ -66,7 +65,13 @@ class image_gui():
 root_main= tkinter.Tk()  
 image_gui(root_main)  
 root_main.title("ファイル名を出力するだけ")  
-root_main.geometry("300x100") 
+root_main.geometry("1000x600") 
+
+textExample=ScrolledText(root_main, height=35,width=120, wrap=tkinter.CHAR)
+textExample.pack()
+textExample.place(x=90, y=70)
+
+textExample.delete("1.0",tkinter.END)
 
 root_main.mainloop()
 
